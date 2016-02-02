1c1026bec5aed922595d3a2262fe2757f189df65
colinlmcdonald <mickydwow@gmail.com>
2016-02-01 16:44:48 -0800

    stuck on blinky dancer step

--- spec/blinkyDancerSpec.js
+++ spec/blinkyDancerSpec.js
@@ -6,7 +6,8 @@ describe("blinkyDancer", function() {
 
   beforeEach(function() {
     clock = sinon.useFakeTimers();
-    blinkyDancer = makeBlinkyDancer(10, 20, timeBetweenSteps);
+
+    blinkyDancer = new MakeBlinkyDancer(10, 20, timeBetweenSteps);
   });
 
   it("should have a jQuery $node object", function(){
@@ -23,13 +24,17 @@ describe("blinkyDancer", function() {
     it("should call step at least once per second", function(){
       sinon.spy(blinkyDancer, "step");
       expect(blinkyDancer.step.callCount).to.be.equal(0);
-    blinkyDancer = makeBlinkyDancer(10, 20, timeBetweenSteps);
+    //  console.log("blinkyDancer.step.callCount #1", blinkyDancer.step.callCount);
+    blinkyDancer = new MakeBlinkyDancer(10, 20, timeBetweenSteps);
       clock.tick(timeBetweenSteps);
 
+    //  console.log("blinkyDancer.step.callCount #2", blinkyDancer.step.callCount);
       expect(blinkyDancer.step.callCount).to.be.equal(1);
 
       clock.tick(timeBetweenSteps);
       expect(blinkyDancer.step.callCount).to.be.equal(2);
+      console.log("blinkyDancer.step.callCount #3", blinkyDancer.step.callCount);
+
     });
   });
 });
--- src/blinkyDancer.js
+++ src/blinkyDancer.js
@@ -1,19 +1,35 @@
-var makeBlinkyDancer = function(top, left, timeBetweenSteps){
-  var blinkyDancer = makeDancer(top, left, timeBetweenSteps);
-
+var MakeBlinkyDancer = function(top, left, timeBetweenSteps){
+  //var blinkyDancer = MakeDancer(top, left, timeBetweenSteps);
+  MakeDancer.call(this);
+  this.$node = $('.addDancerButton');
   // we plan to overwrite the step function below, but we still want the superclass step behavior to work,
   // so we must keep a copy of the old version of this function
+  console.log(this.$node);
+  this.step();
+  this.timeBetweenSteps = timeBetweenSteps;
+//  this.oldStep = this.step;
+};
 
-  var oldStep = blinkyDancer.step;
 
-  blinkyDancer.step = function(){
+  MakeBlinkyDancer.prototype = Object.create(MakeDancer.prototype);
+  MakeBlinkyDancer.prototype.constructor = MakeBlinkyDancer;
+
+  MakeBlinkyDancer.prototype.step = function(){
     // call the old version of step at the beginning of any call to this new version of step
-    oldStep();
+    //this.oldStep();
     // toggle() is a jQuery method to show/hide the <span> tag.
     // See http://api.jquery.com/category/effects/ for this and
     // other effects you can use on a jQuery-wrapped html tag.
-    blinkyDancer.$node.toggle();
+  //  console.log("Value of this within MakeBlinkyDancer.step", this);
+    $('.addDancerButton').toggle();
+
+   MakeDancer.prototype.step.call(this);
   };
 
-  return blinkyDancer;
-};
+
+//console.log(MakeDancer.prototype.step())
+    //var stepper = bind.call(this.$node.toggle());
+    //var boundToggle = this.$node.toggle().bind(this.$node);
+    //console.log(boundToggle);
+    //setTimeout(this.step.bind(this.$node), this.timeBetweenSteps);
+    //setTimeout(boundToggle, this.timeBetweenSteps);
